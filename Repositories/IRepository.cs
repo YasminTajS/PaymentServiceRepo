de971b61d7a7dbac4b371d4b1e8a43efd4976757
YasminTajS <yasmintajs93@gmail.com>
2021-04-17 16:00:05 +0530

    commiting PaymentMicroservice

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentService.Repositories
+{
+    public interface IRepository
+    {
+        public double GetBalance(string creditCardNumber, double creditLimit, double processingCharge);
+        
+    }
+
+}
+
---
+++ Repositories/PaymentRepository.cs
@@ -0,0 +1,33 @@
+﻿using PaymentService.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentService.Repositories
+{
+    public class PaymentRepository : IRepository
+    {
+        //public List<ProcessPayment> products;
+
+        public PaymentRepository()
+        {
+
+        }
+
+        public double GetBalance(string creditCardNumber, double creditLimit, double processingCharge)
+        {
+            double balance = 0;
+            if (processingCharge <= creditLimit)
+            {
+                balance += creditLimit - processingCharge;
+                return balance;
+            }
+            else
+                return -1;
+
+        }
+
+
+    }
+}
